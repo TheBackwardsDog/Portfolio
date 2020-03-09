f9de11b64bd3dd617da52297b3afb746105d6de2
TheBackwardsDog <61918980+TheBackwardsDog@users.noreply.github.com>
2020-03-09 18:57:57 -0400

    Updates

---
+++ precache-manifest.9dd2093e0278518fd4146045654c17ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc793e0f83249154819bdb46eab59a8",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "4b8cb2680b1dd5fbc01b",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "931c044aa6a589639739",
+    "url": "/Portfolio/static/js/2.47f75fcd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Portfolio/static/js/2.47f75fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8cb2680b1dd5fbc01b",
+    "url": "/Portfolio/static/js/main.7a575c36.chunk.js"
+  },
+  {
+    "revision": "1de34f27067344e4efe4",
+    "url": "/Portfolio/static/js/runtime-main.a22efcd1.js"
+  }
+]);
